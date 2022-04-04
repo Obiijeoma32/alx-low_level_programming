02cea60f52a5a8c44745f1db23af77dbb8fc9564
Allen-Codz <ekeneenyi@gmail.com>
2022-04-04 13:05:55 +0100

    new commit for today

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,44 @@
+/*
+ * File: 2-str_concat.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 #include <stdlib.h>
 
 /**
-* str_concat - concatenates two strings.
-* @s1: The first string to print.
-* @s2: The second string to print.
-* Return: The sum of two strings.
-*/
-
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	int i, j = 0;
-	char *p;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	for (a = 0; s1[a] || s2[a]; a++)
-	{
-		b++;
-	}
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-	p = malloc(sizeof(char) * b);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (p == NULL)
-	{
+	if (concat_str == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; s1[i]; i++)
-	{
-		p[j++] = s1[i];
-	}
-	for (i = 0; s2[i]; i++)
-	{
-		p[j++] = s2[i];
-	}
-	return (p);
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
